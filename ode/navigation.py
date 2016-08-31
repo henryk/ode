9c38fc41157f9d34724528038b173711192630fb
Henryk Plötz <henryk@ploetzli.ch>
2016-08-31 17:09:47 +0200

    Take control of visit_Navbar

--- ode/navigation.py
+++ ode/navigation.py
@@ -40,31 +40,60 @@ class ODENavbar(Navbar):
 		self.items = self.items + (UserMenu(),)
 
 class ODENavbarRenderer(BootstrapRenderer):
-	# Right-aligns last item
-	# Fix to top
+
+	# This is a copy of BootstrapRenderer.visit_Navbar, but not as stupid
+	#  + Customize Brand (make dropdown of modules)
+	#  * Fix to top
+	#  * Put UserMenu into a right-aligned menu
 	def visit_Navbar(self, node):
-		import dominate
+		from dominate import tags
+		from hashlib import sha1
+		
+		# create a navbar id that is somewhat fixed, but do not leak any
+		# information about memory contents to the outside
+		node_id = self.id or sha1(str(id(node)).encode()).hexdigest()
 
-		result = super(ODENavbarRenderer, self).visit_Navbar(node)
-		div = None
-		child = None
-		for _ in result.get("div"):
-			if "navbar-collapse" in _['class']:
-				div = _
-				break
-		if div:
+		root = tags.nav() if self.html5 else tags.div(role='navigation')
+		root['class'] = 'navbar navbar-default navbar-fixed-top'
 
-			for bar in div:
-				pass
-			for child in bar:
-				pass
-			if child:
-				bar.remove(child)
-				rightbar = dominate.tags.ul()
-				rightbar['class'] = "nav navbar-nav navbar-right"
-				div.add(rightbar)
-				rightbar.add(child)
+		cont = root.add(tags.div(_class='container-fluid'))
 
-		result['class'] += " navbar-fixed-top"
+		# collapse button
+		header = cont.add(tags.div(_class='navbar-header'))
+		btn = header.add(tags.button())
+		btn['type'] = 'button'
+		btn['class'] = 'navbar-toggle collapsed'
+		btn['data-toggle'] = 'collapse'
+		btn['data-target'] = '#' + node_id
+		btn['aria-expanded'] = 'false'
+		btn['aria-controls'] = 'navbar'
 
-		return result
+		btn.add(tags.span('Toggle navigation', _class='sr-only'))
+		btn.add(tags.span(_class='icon-bar'))
+		btn.add(tags.span(_class='icon-bar'))
+		btn.add(tags.span(_class='icon-bar'))
+
+		header.add(tags.span(node.title, _class='navbar-brand'))
+				
+
+		bar = cont.add(tags.div(
+			_class='navbar-collapse collapse',
+			id=node_id,
+		))
+		bar_list = bar.add(tags.ul(_class='nav navbar-nav'))
+
+		right_items = []
+
+		for item in node.items:
+			if isinstance(item, UserMenu):
+				right_items.append(item)
+			else:
+				bar_list.add(self.visit(item))
+
+		if right_items:
+			right_list = bar.add(tags.ul(_class='nav navbar-nav navbar-right'))
+
+			for item in right_items:
+				right_list.add(self.visit(item))
+
+		return root
