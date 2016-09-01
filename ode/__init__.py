cc2af720527964af8ebd939a03052cbcf36b5d4e
Henryk Plötz <henryk@ploetzli.ch>
2016-09-02 00:59:28 +0200

    Add naming convention to SQLAlchemy metdata

--- ode/__init__.py
+++ ode/__init__.py
@@ -8,6 +8,7 @@ from flask_bootstrap import Bootstrap
 from flask_session import Session
 from flask_ldapconn import LDAPConn
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
 from flask_migrate import Migrate, MigrateCommand
 
 from flask_nav import Nav, register_renderer
@@ -16,8 +17,15 @@ bs = Bootstrap()
 _s = Session()
 ldap = LDAPConn()
 nav = Nav()
-db = SQLAlchemy()
 migrate = Migrate()
+db = SQLAlchemy(metadata=MetaData(naming_convention={
+	"ix": 'ix_%(column_0_label)s',
+	"uq": "uq_%(table_name)s_%(column_0_name)s",
+	"ck": "ck_%(table_name)s_%(constraint_name)s",
+	"fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+	"pk": "pk_%(table_name)s"
+}))
+
 
 def create_app(configuration="ode.config.Config", **kwargs):
 	app = Flask(__name__)
