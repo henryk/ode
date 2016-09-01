42b3e5c6df8539edf36a18cd10cb43b2c349178e
Henryk Plötz <henryk@ploetzli.ch>
2016-09-01 09:20:28 +0200

    Print something more useful

--- ode/blueprints/isi/views.py
+++ ode/blueprints/isi/views.py
@@ -4,8 +4,11 @@ from ode import config_get, session_box, login_required
 from . import blueprint, forms
 from .model import Source
 
-import vobject
+import vobject, pprint
 
+@blueprint.app_template_filter("pprint")
+def pprint_string(s):
+	return pprint.pformat(s)
 
 @blueprint.route("/")
 @login_required(False)
@@ -14,14 +17,26 @@ def root():
 
 	sources = Source.query.all()
 
-	calendars = [vobject.readOne(s.contents) for s in sources]
+	events = []
 
 	# vobject loses the unicode property somewhere along the way so that properties are 'str' object
 	# I *think* they are all UTF-8 encoded, since this is the iCalendar default, so for simple printing
 	# do this:
-	calendars_strings = [unicode(str(c), "UTF-8") for c in calendars]
+	def s_(s):
+		return s if isinstance(s, unicode) else unicode(s, "UTF-8")
 
-	return render_template("isi/root.html", calendars=calendars_strings, refresh_form=refresh_form)
+	for s in sources:
+		calendar = vobject.readOne(s.contents)
+		for event in calendar.vevent_list:
+			events.append({
+				"source": s.name,
+				"summary": s_(event.summary.value),
+				"description": s_(event.description.value),
+				"start": event.dtstart.value,
+				"end": event.dtend.value,
+			})
+
+	return render_template("isi/root.html", events=events, refresh_form=refresh_form)
 
 @blueprint.route("/refresh", methods=["POST"])
 def refresh():
