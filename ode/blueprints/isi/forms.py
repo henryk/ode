9da70540af8506a41f2c2e7153bf40d725d32d90
Henryk Plötz <henryk@ploetzli.ch>
2016-09-02 10:26:07 +0200

    Make sure the recipients field picks up additional recipients

--- ode/blueprints/isi/forms.py
+++ ode/blueprints/isi/forms.py
@@ -1,18 +1,19 @@
 from flask_wtf import Form
 from wtforms import HiddenField, SubmitField, StringField, TextField, FieldList
 from wtforms.widgets import TextArea
+from wtforms.validators import DataRequired
 
 class RefreshForm(Form):
 	refresh = SubmitField('Refresh!')
 
 class CreateInvitationForm(Form):
-	event_id = HiddenField()
+	event_id = HiddenField(validators=[DataRequired()])
 	create = SubmitField('')
 
 class EditInvitationForm(Form):
-	subject = StringField('Subject')
-	recipients_raw = FieldList(StringField("Recipients"))
-	sender = TextField('Sender')
+	subject = StringField('Subject', validators=[DataRequired()])
+	recipients_raw = FieldList(StringField("Recipients"), min_entries=2)
+	sender = TextField('Sender', validators=[DataRequired()])
 	text_html = TextField('Invitation text', widget=TextArea())
 
 	save = SubmitField('Save')
