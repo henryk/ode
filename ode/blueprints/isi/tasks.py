d57cea91c2b7eb5d351f923816ff5bdb08de1a65
Henryk Plötz <henryk@ploetzli.ch>
2016-09-03 20:26:59 +0200

    Add full icalendar attachment

--- ode/blueprints/isi/tasks.py
+++ ode/blueprints/isi/tasks.py
@@ -6,7 +6,7 @@ from flask import current_app, url_for, render_template_string
 
 from flask_mail import Message
 
-import datetime
+import datetime, vobject, flanker.addresslib.address
 
 
 class NonStupidMessage(Message):
@@ -15,7 +15,9 @@ class NonStupidMessage(Message):
 		creates a multipart/alternative with a text/plain and a text/html part.
 		Even if there is no plain part, which is left empty in that case.
 		This class undoes this: If there is no plain part, the alternative part
-		is replaced with the html part."""
+		is replaced with the html part.
+		Also it allows to remove the Content-Disposition header by setting the
+		disposition to REMOVE."""
 	def _message(self):
 		retval = super(NonStupidMessage, self)._message()
 
@@ -32,6 +34,11 @@ class NonStupidMessage(Message):
 					else:
 						p.set_payload(payload_inner)
 			retval.set_payload(payload)
+
+		for part in retval.walk():
+			d = part.get("Content-Disposition", "")
+			if d.startswith("REMOVE"):
+				del part["Content-Disposition"]
 
 		return retval
 
@@ -59,17 +66,33 @@ def send_one_mail(recipient_id):
 
 		invitation = recipient.invitation
 
-		template_params = dict(recipient=recipient, 
+		template_params = dict( 
 			invitation=invitation, event=invitation.event,
 			link_yes=link_yes, link_no=link_no)
 
-		params = dict(sender=invitation.sender,
+		sender_address = flanker.addresslib.address.parse(invitation.sender)
+		recipient_address = flanker.addresslib.address.parse(recipient.pending_address)
+
+		params = dict(sender=sender_address.full_spec(),
 			subject=invitation.subject)
 
 		params["html"] = render_template_string(invitation.text_html, **template_params)
 		params["charset"] = "UTF-8"
 
-		msg = NonStupidMessage(recipients=[recipient.pending_address], **params)
+		cal = vobject.newFromBehavior('vcalendar')
+		cal.vevent=invitation.event._vevent
+		cal.add('method').value="REQUEST"
+		cal.vevent.add('ORGANIZER').value="MAILTO:"+sender_address.address
+		cal.vevent.add('ATTENDEE').value="MAILTO:"+recipient_address.address
+		cal.vevent.attendee.partstat_param="NEEDS-ACTION"
+		cal.vevent.attendee.rsvp_param="TRUE"
+
+		calstream = cal.serialize()
+
+		msg = NonStupidMessage(recipients=[recipient_address.full_spec()], **params)
+		msg.attach("invite.ics", "text/calendar; method=REQUEST", calstream,
+			#disposition="REMOVE",
+		)
 		retval = mailer.send(msg)
 
 		recipient.state = recipient.state.SENT
