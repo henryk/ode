aa32fddf9c32670f5d937702b95ff4977231b30d
devmarcstorm <43650740+devmarcstorm@users.noreply.github.com>
2019-11-26 15:05:16 +0100

    Undo last commit

--- ode/blueprints/amu/birthday_functions.py
+++ ode/blueprints/amu/birthday_functions.py
@@ -2,8 +2,6 @@ from datetime import date, datetime
 import uuid
 import socket
 
-from flask_babel import _
-
 def create_ical(user_list, group_list):    
     event = ''
 
@@ -16,11 +14,12 @@ def create_ical(user_list, group_list):
             if user.dn in group.members:
                 groups += '{} '.format(group.name)
 
-        uuid = "{}-{}-{}@{}".format(str(user.name).replace(' ', '-'), date, 'amu', socket.gethostname().lower())
+        uuid = "{}-{}-{}@{}".format(user.name.replace(' ', '-'), date, 'amu', socket.gethostname().lower())
         if date != '':
             event += 'BEGIN:VEVENT\rSUMMARY:{}\rDTSTART;VALUE=DATE:{}\rUID:{}\rDESCRIPTION:{}\rPRIORITY:5\rRRULE:FREQ=YEARLY;INTERVAL=1\rEND:VEVENT\r'.format(summary, date, str(uuid), groups)
 
     if event != '':
         cal = 'BEGIN:VCALENDAR\r{}END:VCALENDAR'.format(event)
         return cal
-    return None
+    return None 
+    
